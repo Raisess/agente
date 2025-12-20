e5b6d888047d30aac05e4065ff34405502e78dd6
Raisess <raisechif@gmail.com>
2025-12-20 16:58:57 -0300

    refactor: migrate to separate runtime format

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 use std::collections::HashMap;
 use std::sync::Arc;
 
-use agente::prompt::prompt;
+use agente::runtime::Runtime;
 use agente_application::tools::{read::ReadTool, write::WriteTool};
 use agente_domain::core::tool::Tool;
-use agente_domain::ports::agent::{Agent, FeedResponse, MessageRequest};
 use agente_infrastructure::adapters::agents::chat_gpt::ChatGPT;
 use agente_infrastructure::adapters::file_system::FileSystem;
 
@@ -12,76 +11,13 @@ use agente_infrastructure::adapters::file_system::FileSystem;
 async fn main() {
     let fs = Arc::new(FileSystem::default());
 
-    let mut tools = HashMap::<&str, Box<dyn Tool>>::new();
-    tools.insert("Read", Box::new(ReadTool::new(fs.clone())));
-    tools.insert("Write", Box::new(WriteTool::new(fs.clone())));
+    let mut tools = HashMap::<String, Box<dyn Tool>>::new();
+    tools.insert("Read".to_string(), Box::new(ReadTool::new(fs.clone())));
+    tools.insert("Write".to_string(), Box::new(WriteTool::new(fs.clone())));
 
     let api_key =
         std::env::var("CHAT_GPT_API_KEY").expect("CHAT_GPT_API_KEY to be set");
-    let mut agent = ChatGPT::new(String::from(api_key));
-    let initial_feed_response = agent
-        .feed(MessageRequest {
-            previous_message_id: None,
-            prompt: prompt(&tools),
-        })
-        .await
-        .expect("Failed to setup agent initial prompt.");
-
-    let mut feed_result = FeedResponse::default();
-    loop {
-        let mut input = String::new();
-        println!("Prompt: ");
-        std::io::stdin()
-            .read_line(&mut input)
-            .expect("Should have a input");
-
-        let execution_plan = agent
-            .ask(MessageRequest {
-                previous_message_id: initial_feed_response.message_id.clone(),
-                prompt: input.trim().to_string(),
-            })
-            .await
-            .expect("Failed to ask the agent for the execution plan.");
-        println!("RESPONSE: {execution_plan:#?}");
-
-        for task in execution_plan {
-            println!("Summary: {}", task.summary());
-
-            let key = task.tool();
-            let tool = tools
-                .get(&key.as_str())
-                .expect(&format!("Tool not found: {key}"));
-
-            // @FIXME: improve this hardcoded ugly thing here
-            let mut args = task.arguments();
-            if key == "Write"
-                && args.get(1).is_some_and(|value| value == "<NONE>")
-            {
-                args[1] = feed_result.content.clone();
-            }
-
-            let result = tool.handle(args).await;
-            println!("{key}: {result:#?}");
-            match result {
-                Ok(mut message) => {
-                    if let Some(usage_instruction) = tool.usage_instruction() {
-                        message = format!("{usage_instruction}: {message}");
-                    }
-
-                    feed_result = agent
-                        .feed(MessageRequest {
-                            previous_message_id: None,
-                            prompt: message,
-                        })
-                        .await
-                        .expect(
-                            "Failed to feed agent with tool result \
-                             information.",
-                        );
-                    println!("FEED RESULT: {:#?}", feed_result);
-                }
-                Err(error) => eprintln!("{}", error.message()),
-            }
-        }
-    }
+    let agent = ChatGPT::new(String::from(api_key));
+    let mut runtime = Runtime::init(Box::new(agent), tools);
+    runtime.run().await;
 }
---
+++ src/runtime.rs
@@ -0,0 +1,96 @@
+use std::collections::HashMap;
+
+use agente_domain::core::tool::Tool;
+use agente_domain::ports::agent::{Agent, FeedResponse, MessageRequest};
+
+use crate::prompt::prompt;
+
+pub struct Runtime {
+    agent: Box<dyn Agent>,
+    tools: HashMap<String, Box<dyn Tool>>,
+}
+
+impl Runtime {
+    pub fn init(
+        agent: Box<dyn Agent>,
+        tools: HashMap<String, Box<dyn Tool>>,
+    ) -> Self {
+        Self { agent, tools }
+    }
+
+    pub async fn run(&mut self) -> () {
+        let initial_feed_response = self
+            .agent
+            .feed(MessageRequest {
+                previous_message_id: None,
+                prompt: prompt(&self.tools),
+            })
+            .await
+            .expect("Failed to setup agent initial prompt.");
+
+        let mut feed_result = FeedResponse::default();
+        loop {
+            let mut input = String::new();
+            println!("Prompt: ");
+            std::io::stdin()
+                .read_line(&mut input)
+                .expect("Should have a input");
+
+            let execution_plan = self
+                .agent
+                .ask(MessageRequest {
+                    previous_message_id: initial_feed_response
+                        .message_id
+                        .clone(),
+                    prompt: input.trim().to_string(),
+                })
+                .await
+                .expect("Failed to ask the agent for the execution plan.");
+            println!("RESPONSE: {execution_plan:#?}");
+
+            for task in execution_plan {
+                println!("Summary: {}", task.summary());
+
+                let key = task.tool();
+                let tool = self
+                    .tools
+                    .get(&key)
+                    .expect(&format!("Tool not found: {key}"));
+
+                // @FIXME: improve this hardcoded ugly thing here
+                let mut args = task.arguments();
+                if key == "Write"
+                    && args.get(1).is_some_and(|value| value == "<NONE>")
+                {
+                    args[1] = feed_result.content.clone();
+                }
+
+                let result = tool.handle(args).await;
+                println!("{key}: {result:#?}");
+                match result {
+                    Ok(mut message) => {
+                        if let Some(usage_instruction) =
+                            tool.usage_instruction()
+                        {
+                            message = format!("{usage_instruction}: {message}");
+                        }
+
+                        feed_result = self
+                            .agent
+                            .feed(MessageRequest {
+                                previous_message_id: None,
+                                prompt: message,
+                            })
+                            .await
+                            .expect(
+                                "Failed to feed agent with tool result \
+                                 information.",
+                            );
+                        println!("FEED RESULT: {:#?}", feed_result);
+                    }
+                    Err(error) => eprintln!("{}", error.message()),
+                }
+            }
+        }
+    }
+}
