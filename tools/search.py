98d35a7dfc92764a80a6c9df54e18d641352691a
Raisess <raisechif@gmail.com>
2026-05-09 18:28:12 -0300

    feat: add search tool for web search url listing and content resume

---
+++ tools/search.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python3
+
+import argparse
+
+from ddgs import DDGS
+
+def search(query):
+  output = ""
+  with DDGS() as ddgs:
+    for r in ddgs.text(query, max_results=5):
+      output += "Title: " + r["title"] + "\n" + "Description: " + r["body"] + "\n" + "URL: " + r["href"] + "\n\n"
+
+  print(output.strip())
+
+# Create command-line interface
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(description='Search the web with a query')
+  parser.add_argument('--query', type=str, help='Query for the search engine')
+  args = parser.parse_args()
+  
+  search(args.query)
