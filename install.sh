7bfe1abb33e08e4b926862a64692d9d12beb96f4
Raisess <raisechif@gmail.com>
2026-05-09 18:41:06 -0300

    fix: install script

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 # Copy the tools to $HOME/.agente
 mkdir $HOME/.agente
-cp /home/lara/code/agente/tools.json $HOME/.agente/
-cp -r /home/lara/code/agente/tools $HOME/.agente/
-cp -r /home/lara/code/agente/prompts $HOME/.agente/
+cp $PWD/tools.json $HOME/.agente/
+cp -r $PWD/tools $HOME/.agente/
+cp -r $PWD/prompts $HOME/.agente/
 
 # Builds and install the project
 cargo install --path .
