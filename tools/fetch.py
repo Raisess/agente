8a530e9580977fe59b070e28c4ba3bda039c5fc8
Raisess <raisechif@gmail.com>
2026-05-11 17:42:14 -0300

    fix: fetch tool when no html is retrieved

--- tools/fetch.py
+++ tools/fetch.py
@@ -4,14 +4,17 @@ import argparse
 import trafilatura
 
 def fetch(url: str) -> None:
-  html = trafilatura.fetch_url(url=url)
-  output = trafilatura.extract(filecontent=html, url=url)
-  print(output)
-
+    html = trafilatura.fetch_url(url=url)
+    # Verificar se o HTML foi recuperado com sucesso
+    if html:
+        output = trafilatura.extract(filecontent=html, url=url)
+        print(output)
+    else:
+        print('Failed to fetch HTML content from the provided URL.')
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser(description="Fetch a web page using the url")
-  parser.add_argument("--url", type=str, help="URL for search")
-  args = parser.parse_args()
+    parser = argparse.ArgumentParser(description="Fetch a web page using the url")
+    parser.add_argument("--url", type=str, help="URL for search")
+    args = parser.parse_args()
 
-  fetch(args.url)
+    fetch(args.url)
