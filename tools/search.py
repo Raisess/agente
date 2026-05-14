1005d463263c60ced5a9c6b2ba91ca481906d1b9
Raisess <raisechif@gmail.com>
2026-05-14 15:43:46 -0300

    fix: remove `safe_search` from search tool

--- tools/search.py
+++ tools/search.py
@@ -7,7 +7,7 @@ from ddgs import DDGS
 def search(query: str) -> None:
   output = ""
   with DDGS() as ddgs:
-    for r in ddgs.text(query, max_results=5):
+    for r in ddgs.text(query, max_results=5, safe_search=False):
       output += "Title: " + r["title"] + "\n" + "Description: " + r["body"] + "\n" + "URL: " + r["href"] + "\n\n"
 
   print(output.strip())
